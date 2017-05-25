e9c4051ccbc0657a78d86e53f6a14bb94a2c0bd1
tagliati <andre.tagliati@netshoes.com>
2017-05-25 18:47:03 -0300

    m[e

--- js/spa.js
+++ js/spa.js
@@ -5,10 +5,7 @@ $(document).ready(function() {
 		var href = $(this).attr("href"); // modify the selector here to change the scope of intercpetion
 		console.log("ops");
 	
-		 // Push this URL "state" onto the history hash.
-		$.bbq.pushState(href,2);
 
-		// Prevent the default click behavior.
 		return false;
 	});
 });
